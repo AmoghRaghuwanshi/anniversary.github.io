5c19be22e60c6a783e775cdfdde8f08c952a2225
AmoghRaghuwanshi <amoghraghuwanshi@gmail.com>
2026-05-22 23:27:04 +0530

    fix: update base path for renamed repo

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   // IMPORTANT: Change '/anniversary/' to match your GitHub repo name
-  base: '/-anniversary/',
+  base: '/anniversary.github.io/',
   plugins: [react(), tailwindcss()],
 })
